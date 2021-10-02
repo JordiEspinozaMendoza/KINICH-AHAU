b052fb18ab870ec24c1bf1cc78096c5b74906504
Alexander Carrillo <alexandergcca@gmail.com>
2021-10-02 13:08:43 -0500

    new changes

--- kinich-backend/server/index.js
+++ kinich-backend/server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const morgan = require("morgan");
 
 const app = express();
 const port = 5000;
@@ -10,8 +11,9 @@ app.listen(port, () => {
     console.log(`Servidor corriendo en el puerto ${port}`)
 })
 
-app.use(cors());
+app.use(morgan("dev"));
 app.use(express.json());
+app.use(cors());
 
 app.use("/", routes);
 
