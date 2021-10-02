36d186fe4e88a141758a9552c32a32683fec7525
Alexander Carrillo <alexandergcca@gmail.com>
2021-10-02 18:37:03 -0500

    ready

--- kinich-backend/server/controller/daily.js
+++ kinich-backend/server/controller/daily.js
@@ -1,7 +1,19 @@
+const axios = require("axios");
+
 const getPointDaily = async (req, res) => {
 
     try {
 
+        const { longitude, latitude, start, end } = req.body;
+
+        const urlBase = "https://power.larc.nasa.gov/api/temporal/daily/point?parameters=T2M&community=SB&"
+        const urlComplete = `${urlBase}longitude=${longitude}&latitude=${latitude}&start=${start}&end=${end}&format=JSON`
+
+        console.log(urlComplete);
+
+        const { data } = await axios.get(urlComplete, { crossdomain: true });
+
+        res.status(201).send(data);
     } catch (error) {
         return res.status(404).send(error.message);
     }
--- kinich-backend/server/controller/hourly.js
+++ kinich-backend/server/controller/hourly.js
@@ -1,7 +1,22 @@
+const axios = require("axios");
+
 const getPointHourly = async (req, res) => {
 
     try {
 
+        const { longitude, latitude, start, end } = req.body;
+
+        const urlBase = "https://power.larc.nasa.gov/api/temporal/hourly/point?parameters=T2M&community=SB&"
+        const urlComplete = `${urlBase}longitude=${longitude}&latitude=${latitude}&start=${start}&end=${end}&format=JSON`
+
+        //20170101
+        // 20170102
+
+        console.log(urlComplete);
+
+        const { data } = await axios.get(urlComplete, { crossdomain: true });
+
+        res.status(201).send(data);
     } catch (error) {
         return res.status(404).send(error.message);
     }
